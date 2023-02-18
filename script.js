e0de8fb1f349acdcfbb190b43dc1eb1bea21d186
Forkeh <forkeh@gmail.com>
2023-02-18 16:30:21 +0100

    Can now click orcs so they play death animation

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let countdown = 10;
 
+// ---------- Countdown ---------- //
+
 function countDown() {
   for (let i = countdown; i >= 0; i--) {
     setTimeout(() => {
@@ -11,3 +13,14 @@ function countDown() {
   }
 }
 // countDown();
+
+document.querySelector("#orc_sprite").addEventListener("click", orcClick);
+
+function orcClick() {
+  const container = document.querySelector("#orc_container");
+  const sprite = document.querySelector("#orc_sprite");
+
+  container.classList.add("pauseAnimation");
+  sprite.classList.add("orc_death");
+  sprite.setAttribute("src", "images/Orc/orc_death.png");
+}
