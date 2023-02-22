b704b7691c6a28e67ef06dc4ac8f806cfa432906
Forkeh <forkeh@gmail.com>
2023-02-22 18:31:23 +0100

    Added swanky animations to mana icons

--- manaIcons.js
+++ manaIcons.js
@@ -10,12 +10,30 @@ function decrementMana() {
 function fullMana() {
   mana = 4;
   document.querySelector("#mana1").classList.remove("inactive_mana");
-  document.querySelector("#mana1").classList.add("active_mana");
+  document.querySelector("#mana1").classList.add("full_mana");
   document.querySelector("#mana2").classList.remove("inactive_mana");
-  document.querySelector("#mana2").classList.add("active_mana");
+  document.querySelector("#mana2").classList.add("full_mana");
   document.querySelector("#mana3").classList.remove("inactive_mana");
-  document.querySelector("#mana3").classList.add("active_mana");
+  document.querySelector("#mana3").classList.add("full_mana");
   document.querySelector("#mana4").classList.remove("inactive_mana");
-  document.querySelector("#mana4").classList.add("active_mana");
+  document.querySelector("#mana4").classList.add("full_mana");
+
+  document.querySelector("#mana1").addEventListener('animationend', activeMana);
+  document.querySelector("#mana2").addEventListener("animationend", activeMana);
+  document.querySelector("#mana3").addEventListener("animationend", activeMana);
+  document.querySelector("#mana4").addEventListener("animationend", activeMana);
+
   // console.log("FULL MANA: " + mana);
+}
+
+function activeMana(){
+  // console.log('ACTIVE MANA');
+document.querySelector("#mana1").classList.remove("full_mana");
+document.querySelector("#mana1").classList.add("active_mana");
+document.querySelector("#mana2").classList.remove("full_mana");
+document.querySelector("#mana2").classList.add("active_mana");
+document.querySelector("#mana3").classList.remove("full_mana");
+document.querySelector("#mana3").classList.add("active_mana");
+document.querySelector("#mana4").classList.remove("full_mana");
+document.querySelector("#mana4").classList.add("active_mana");
 }
